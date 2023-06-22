867a24eb112eb1ea4a6bf004ca21e80534f5f22b
Gachara <bgachara@gmail.com>
2023-06-22 17:46:46 +0300

    instruction execution and control flow

--- src/main.rs
+++ src/main.rs
@@ -57,8 +57,42 @@ impl Registers {
     }
 }
 
+enum JumpTest {
+    NotZero,
+    Zero,
+    NotCarry,
+    Carry,
+    Always
+}
+
 enum Instruction {
     ADD(ArithmeticTarget),
+    JP(JumpTest),
+}
+
+impl Instruction {
+    fn from_byte(byte: u8, prefixed: bool) -> Option<Instruction> {
+        if prefixed {
+            Instruction::from_byte_prefixed(byte)
+        }else {
+            Instruction::from_byte_not_prefixed(byte)
+            }
+        }
+    }
+    
+    fn from_byte_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+            0x00 => Some(Instruction::RLC(PrefixTarget::B)),
+            _ => /* ADD THE REST OF INSTRUCTION*/ None
+    }
+}
+
+    fn from_byte_not_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+        0x02 => Some(Instruction::INC(IncDecTarget::BC)),
+        _ => /* map the rest of the instructions*/ None
+    }
+}
 }
 
 enum ArithmeticTarget {
@@ -71,29 +105,97 @@ enum ArithmeticTarget {
     L,
 }
 
-impl CPU {
-    fn execute(&mut self, instruction: Instruction) {
-        match Instruction {
-            Instruction::ADD(target) => match target {
-                ArithmeticTarget::C => {
-                    let value = self.registers.c;
-                    let new_value = self.add(value);
-                    self.registers.a = new_value;
-                }
-            _ => { /* This is another comment*/}
-        }
+struct CPU {
+    registers: Registers,
+    pc: u16,
+    bus: MemoryBus,
+}
+
+struct MemoryBus {
+    memory: [u8; 0xFFFF],
+}
+
+impl MemoryBus {
+    fn read_byte(&self, address: u16) -> u8 {
+        self.memory[address as usize]
     }
-    _ => {/* THis is a comment not JSON*/ }
 }
 
-fn add(&mut self, value:u8) -> u8 {
-    let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
-    self.registers.f.zero = new_value = 0;
-    self.registers.f.subtract = false;
-    self.registers.f.carry = did_overflow;
-        
-    self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
-    new_value
-}
-}
+impl CPU {
+    fn execute(&mut self, instruction: Instruction) -> u16 {
+        match instruction {
+            Instruction::ADD(target) => {
+                match target {
+                    ArithmeticTarget::C => {
+                        let value = self.registers.c;
+                        let new_value = self.add(value);
+                        self.registers.a = new_value;
+                        self.pc.wrapping_add(1)
+                    }
+                    _ => {
+                        /* This is another comment*/
+                        self.pc
+                    }
+                }
+            }
+            _ => {
+                /* THis is a comment not JSON*/
+                self.pc
+            }
+        }
+        Instruction::JP(test) => {
+         let jump_condition = match test {
+                JumpTest::NotZero => !self.registers.f.zero,
+                JumpTest::NotCarry => !self.registers.f.carry,
+                JumpTest::Zero => !self.registers.f.zero,
+                JumpTest::Carry => !self.registers.f.carry,
+                JumpTest::Always => true
+            };
+            self.jump(jump_condition)
+    }
+        _ => { /*add other implementations*/ self.pc}
+    } 
+    }
 
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        self.registers.f.zero = new_value = 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = did_overflow;
+
+        self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
+        new_value
+    }
+
+    fn step(&mut self) {
+        let mut instruction_byte = self.bus.read_byte(self.pc);
+        let prefixed = instruction_byte == 0xCB;
+        if prefixed {
+            instruction_byte = self.bus.read_byte(self.pc + 1);
+        }
+
+        let next_pc = if let Some(instruction) = Instruction::from_byte(instruction_byte, prefixed)
+        {
+            self.execute(instruction)
+        } else {
+            let description = format!(
+                "0x{}{:x}",
+                if prefixed { "cb" } else { "" },
+                instruction_byte
+            );
+            panic!("unknown instruction", instruction_byte);
+        };
+
+        self.pc = next_pc;
+    }
+
+    fn jump(&self, should_jump: bool) -> u16 {
+        if should_jump {
+        let least_significant_byte = self.bus.read_byte(self.pc + 1) as u16;
+        let most_significant_byte = self.bus.read_byte(self.pc + 1) as u16;
+        (most_significant_byte << 8) | least_significant_byte 
+    } else {
+        self.pc.wrapping_add(3)
+    }
+}
+}
